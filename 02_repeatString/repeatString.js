bc28ba91c20538bea0fcbf260ea5e27d5a56ad43
michaelsdon <michael.i.elsdon.online@gmail.com>
2022-11-06 21:29:34 -0700

    Completed exercise 01, 02, and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  new_string = '';
 
+  if (num < 0) {
+    return 'ERROR';
+  };
+
+  for (let i = num; i > 0; i--) {
+    new_string += string;
+  };
+
+  return new_string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+  
+  var backwardsString = "";
 
+  for (let i = inputString.length - 1; i >= 0; i--) {
+    backwardsString += inputString[i];
+  };
+
+  return backwardsString;
 };
 
 // Do not edit below this line
